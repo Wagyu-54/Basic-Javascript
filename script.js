b7ac8b65904c32b54345320563c85e9083e2d862
Wagyu-54 <eman54.ew@gmail.com>
2026-02-17 17:24:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,38 @@ function runForLoop() {
 
     document.getElementById("forLoopResult").textContent = result.trim();
 }
+/*  Repetition with Condition */
+
+function showOddNumbers() {
+
+    let result = "";
+
+    for (let i = 1; i <= counter; i++) {
+
+        if (i % 2 !== 0) {
+            result += i + " ";
+        }
+    }
+
+    document.getElementById("oddNumberResult").textContent = result.trim();
+}
+
+/*  Arrays */
+
+function addMultiplesToArray() {
+
+    let arr = [];
+
+    if (counter >= 5) {
+
+        for (let i = counter; i >= 5; i--) {
+
+            if (i % 5 === 0) {
+                arr.push(i);
+            }
+        }
+    }
+
+    // Print array to console
+    console.log(arr);
+}
