7b071d92067815cc7219f85b3020e53354ab1a67
Wagyu-54 <eman54.ew@gmail.com>
2026-02-17 17:23:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// Counter variable
+let counter = 0;
 
+// Update counter display
+function updateCounter() {
+    document.getElementById("counter").textContent = counter;
+}
+
+function tickUp() {
+    counter++;
+    updateCounter();
+}
+
+function tickDown() {
+    counter--;
+    updateCounter();
+}
+
+function runForLoop() {
+
+    let result = "";
+
+    for (let i = 0; i <= counter; i++) {
+        result += i + " ";
+    }
+
+    document.getElementById("forLoopResult").textContent = result.trim();
+}
